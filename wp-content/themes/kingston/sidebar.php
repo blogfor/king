11bab3eccff3a8917bce78feab8b0e0191685806
blogfordevelopers <bikash.pal2000@gmail.com>
2016-01-03 14:02:59 +0530

    sidebar.php

--- wp-content/themes/kingston/sidebar.php
+++ wp-content/themes/kingston/sidebar.php
@@ -29,7 +29,7 @@
 
         <div class="col-lg-12 col-md-12 col-sm-12">
             <div class="map"><h2>WHERE WE ARE</h2>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1841.4082897255228!2d88.41423239882693!3d22.623323599467565!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f89f417450d265%3A0xa875f457f8457a71!2sKingston+Educational+Institute!5e0!3m2!1sen!2sin!4v1451239549427" width="320px" height="268px" frameborder="0" style="border:0" allowfullscreen></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1841.4082897255228!2d88.41423239882693!3d22.623323599467565!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f89f417450d265%3A0xa875f457f8457a71!2sKingston+Educational+Institute!5e0!3m2!1sen!2sin!4v1451239549427" width="100%" height="268px" frameborder="0" style="border:0" allowfullscreen></iframe>
 				
 				</div>
         </div>
